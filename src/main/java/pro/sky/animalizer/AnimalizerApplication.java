165b0a42339ea49d61ebb63a5f8ff7940f04acee
eduard <eduard42rus@mail.ru>
2023-10-18 22:22:04 +0700

    init commit

---
+++ src/main/java/pro/sky/animalizer/AnimalizerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.animalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalizerApplication.class, args);
+	}
+
+}
