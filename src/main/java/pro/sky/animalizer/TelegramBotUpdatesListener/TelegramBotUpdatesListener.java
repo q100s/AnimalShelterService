c555100a785ce8f3253ce301052080360dd10a92
Mark <voronkov1mark@gmail.com>
2023-10-26 04:47:28 +0300

    second try(ReTry)

--- src/main/java/pro/sky/animalizer/TelegramBotUpdatesListener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animalizer/TelegramBotUpdatesListener/TelegramBotUpdatesListener.java
@@ -75,6 +75,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     }
 
     private void sendMessageWithButtons(long chatId) {
+        catOrDog.catDogInLineKeyBoard(chatId);
 
 
 
