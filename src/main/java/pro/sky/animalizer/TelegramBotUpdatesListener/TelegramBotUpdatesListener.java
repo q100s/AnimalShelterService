eb0ca720d4052b7e9b8b090095e331d1550ea4ab
Mark <voronkov1mark@gmail.com>
2023-10-29 00:44:59 +0300

    The bot starts, a starting message with a description and shelter selection buttons are displayed, when choosing a shelter, 4 stub buttons are displayed

--- src/main/java/pro/sky/animalizer/TelegramBotUpdatesListener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animalizer/TelegramBotUpdatesListener/TelegramBotUpdatesListener.java
@@ -1,98 +1,91 @@
 package pro.sky.animalizer.TelegramBotUpdatesListener;
+
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
-
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import com.pengrad.telegrambot.request.SendMessage;
-import pro.sky.animalizer.Buttons.CatOrDog;
 
-
-import javax.annotation.PostConstruct;
 import java.util.List;
+
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-
-
-
-
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
     private final TelegramBot telegramBot;
 
-    private final CatOrDog catOrDog;
-
-    public TelegramBotUpdatesListener(TelegramBot telegramBot, CatOrDog catOrDog) {
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
-        this.catOrDog = catOrDog;
     }
-
-
 
     @PostConstruct
     public void init() {
         telegramBot.setUpdatesListener(this);
     }
 
-
     @Override
     public int process(List<Update> updates) {
+        logger.info("Bot is started");
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
 
-            long chatId = update.message().chat().id();
-            String name = update.message().chat().firstName();
+            Long chatId = null;
+            String text = null;
+            if (update.message() != null) {
+                chatId = update.message().chat().id();
+                text = update.message().text();
+            }
+            if (update.callbackQuery() != null) {
+                text = update.callbackQuery().data();
+                chatId = update.callbackQuery().message().chat().id();
+            }
+            if (update.message() != null && update.message().text().equals("/start")) {
+                telegramBot.execute(new SendMessage(chatId, "Hello! I'm an animal shelter's bot."));
+                logger.info("The start message was been sent");
+                InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                        new InlineKeyboardButton[]{
+                                new InlineKeyboardButton("Cat's shelter").callbackData("Cat's shelter"),
+                                new InlineKeyboardButton("Dog's shelter").callbackData("Dog's shelter")
+                        });
+                telegramBot.execute(new SendMessage(chatId, "Please, pick the shelter").replyMarkup(inlineKeyboard));
+                logger.info("shelter selection buttons sent");
+            }
+            assert text != null;
+            if (text.equalsIgnoreCase("Cat's shelter")) {
+                logger.info("cat shelter button pressed");
+                InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                        new InlineKeyboardButton[]{
+                                new InlineKeyboardButton("but1").callbackData("but1"),
+                                new InlineKeyboardButton("but2").callbackData("but2"),
+                                new InlineKeyboardButton("but3").callbackData("but3"),
+                                new InlineKeyboardButton("but4").callbackData("but4")
+                        });
+                telegramBot.execute(new SendMessage(chatId, "Please, pick button for cats shelter ").replyMarkup(inlineKeyboard));
 
-            if (update.message().text() != null && update.message().text().equals("/start")){
-
-                // приветственное сообщение
-                startMessage(chatId,name);
 
             }
-            if(update.message().text().equals("/help")){
-                //  выбор приюта для кошек или собак
-                sendMessageWithButtons(chatId);
-
-
+            if (text.equalsIgnoreCase("Dog's shelter")) {
+                logger.info("dog shelter button pressed");
+                InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                        new InlineKeyboardButton[]{
+                                new InlineKeyboardButton("but1").callbackData("but1"),
+                                new InlineKeyboardButton("but2").callbackData("but2"),
+                                new InlineKeyboardButton("but3").callbackData("but3"),
+                                new InlineKeyboardButton("but4").callbackData("but4")
+                        });
+                telegramBot.execute(new SendMessage(chatId, "Please, pick button for dogs shelter ").replyMarkup(inlineKeyboard));
             }
-
 
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    private void startMessage(long chatId, String name){
-
-        String answer = "Hi, " + name + ", nice to meet you!";
-        sendMessage(chatId, answer);
-    }
-
-    private void sendMessage(long chatId, String answer) {
-        telegramBot.execute(new SendMessage(String.valueOf(chatId), answer));
-
-    }
-
-    private void sendMessageWithButtons(long chatId) {
-        catOrDog.catDogInLineKeyBoard(chatId);
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
 
 }
-
 
 
 
