9c777c470688978499be3bbfc1a229c3c5c83b87
madahtoxa <madahtoxa@mail.ru>
2023-11-12 14:17:44 +0300

    Добавил: 1 исключение для Pet 2 model Pet 3 Pet Repository 4 Pet Service
    Дополнительно накидал набросок по кинологам в Pet Service

--- src/test/java/pro/sky/animalizer/service/PetServiceTest.java
+++ src/test/java/pro/sky/animalizer/service/PetServiceTest.java
@@ -2,16 +2,22 @@ package pro.sky.animalizer.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import pro.sky.animalizer.model.Pet;
 import pro.sky.animalizer.repositories.PetRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class PetServiceTest {
+
+    @Autowired
+    private PetRepository petRepository;
 
 
     @Autowired
@@ -29,28 +35,26 @@ public class PetServiceTest {
     }
 
     /**
-     Тест на получение всех Питомцев. Допилить!!!
+     Тест на редактирование Питомца.
      */
     @Test
-    public void testGetAllPets() {
-
-        Pet p1 = new Pet(1L, "1", "1");
-        Pet p2 = new Pet(2L, "2", "2");
-
+    public void editPet() {
+        Pet p1 = new Pet(1L, "dog", "Rex");
+        Pet p2 = new Pet(1L, "dog", "Rex");
+        p2.setId(2L);
+        Assertions.assertThat(p1).isNotEqualTo(p2);
     }
     /**
-     Тест на создание Питомца. Ошибка в тесте говорит о том что он работает корректно. 
+     Тест на создание Питомца. Ошибка в тесте говорит о том что он работает корректно.
      */
     @Test
     public void testCreatePet() {
-
         Pet p1 = new Pet();
         Assertions.assertThat(p1).hasAllNullFieldsOrPropertiesExcept();
         p1.setId(1L);
         p1.setpetName("Test");
         p1.setPetType("cat");
         Assertions.assertThat(p1).hasAllNullFieldsOrPropertiesExcept();
-
     }
 
 
